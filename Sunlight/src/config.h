4505c2db38aba71b13a03f9f00d4ee3093e23c64
gareth.evans <agrath@gmail.com>
2020-03-31 18:26:19 +1300

    Wifi credentials placeholder

--- Sunlight/src/config.h
+++ Sunlight/src/config.h
@@ -1,8 +1,12 @@
+#include "credentials.h"
 #ifndef CONFIG_H
 
-    #define CONFIG_H
-    #define NUM_LEDS 24
-    #define SERIAL_OUTPUT true
-    const bool debugEnabled = true;
+#define CONFIG_H
+#define NUM_LEDS 24
+#define SERIAL_OUTPUT true
+const bool debugEnabled = true;
+
+const char HardcodedWifi_SSID[] = WIFI_SSID;
+const char HardcodedWifi_Password[] = WIFI_PASSWORD;
 
 #endif
